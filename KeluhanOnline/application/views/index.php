4c5bc104031bf67da69f893f9de05170e38e8131
evimila <evimila137@gmail.com>
2016-12-23 06:16:44 +0700

    ubah gambar

--- KeluhanOnline/application/views/index.php
+++ KeluhanOnline/application/views/index.php
@@ -52,7 +52,7 @@ Hello, <b><?php echo $pengguna->name;?></b>
 	</div>
 	</div>
 	<div class="fadein">
-	<img src="<?php echo base_url('image/mlg.jpg'); ?>" width="100%">
+	<img src="<?php echo base_url('image/satuuser.jpg'); ?>" width="100%">
 	<img src="http://chictraveler.wpengine.netdna-cdn.com/wp-content/uploads/2010/09/Picture-91-500x332.png">
 	<img src="http://sevillespain.info/wp-content/uploads/2012/02/Casa-De-Carmona-Hotel-Seville-casa_de_carmona_galeria_mr_1200-500x332.jpg">
 	<img src="http://hereelsewhere.com/wp-content/uploads/2012/06/Chambre22-500x332.jpeg">
