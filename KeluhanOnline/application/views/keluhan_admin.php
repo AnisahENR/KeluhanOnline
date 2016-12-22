e63d5bb2172ae8256f0f26d7061c95c649e6c43d
DESKTOP-E05UOIF\Anisah Denis <anisahdenis.1595@gmail.com>
2016-12-22 23:50:25 +0700

    tambahan link ke data user pada admin

--- KeluhanOnline/application/views/keluhan_admin.php
+++ KeluhanOnline/application/views/keluhan_admin.php
@@ -23,7 +23,7 @@ header("location: index");
 			<ul id="menu">
 			<li><?php echo anchor(base_url().'index.php/admin/index','Beranda');?></li>
 			<li><?php echo anchor(base_url().'index.php/admin/keluhan_admin','Keluhan');?></li>
-			<li><?php echo anchor(base_url().'#','Data User');?></li>
+			<li><?php echo anchor(base_url().'index.php/admin/data_user','Data User');?></li>
 			</ul>
 		</div>
 	<br>
